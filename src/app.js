1fb77d47908041c24b797e9f26880936db95dcd0
salimtaiyo <ichigeki.ccCsc@hotmail.com>
2018-03-29 12:05:42 -0400

    added style and functionality

--- src/app.js
+++ src/app.js
@@ -1,4 +1,34 @@
-var x = 0;
-const shalom = "3232";
+$(() => {
+  const $dice = $('.dice');
+  const $roll = $('.roll');
+  const $hold = $('.hold');
+  const $playerScoreOne = $('.player--1--roll');
+  const $playerScoreTwo = $('.player--2--roll');
+  let active = 1;
+  let round = 0;
+
+  // player change function
+  function playerChange() {
+    active === 1 ? active = 2 : active = 1;
+  }
+
+  // roll button click event
+  $roll.on('click', () => {
+    // random number generator
+    const random = Math.floor(Math.random() * 3) + 1;
+    $dice.attr('src', `./img/dice-${random}.png`);
+    $dice.show();
+    if (random !== 1) {
+      round += random;
+      $(`.player--${active}--roll`).text(round);
+    } else {
+      playerChange();
+    }
+  });
 
 
+  // hold button click event
+  $hold.on('click', () => {
+    
+  });
+});
