08929b279180306b8eb2e16b3ad260ce32861112
salimtaiyo <ichigeki.ccCsc@hotmail.com>
2018-03-29 18:24:07 -0400

    fourth commit: added background clip and more functionality

--- src/app.js
+++ src/app.js
@@ -2,33 +2,45 @@ $(() => {
   const $dice = $('.dice');
   const $roll = $('.roll');
   const $hold = $('.hold');
-  const $playerScoreOne = $('.player--1--roll');
-  const $playerScoreTwo = $('.player--2--roll');
-  let active = 1;
-  let round = 0;
+  const $playerScoreOne = $('.player--0--roll');
+  const $playerScoreTwo = $('.player--1--roll');
+  let active = 0;
+  let roundScore = 0;
+  const points = [0, 0];
 
-  // player change function
+  // change player
   function playerChange() {
-    active === 1 ? active = 2 : active = 1;
+    active === 0 ? active = 1 : active = 0;
+    roundScore =0;
+    $playerScoreOne.text('0');
+    $playerScoreTwo.text('0');
   }
 
-  // roll button click event
+  // roll button 
   $roll.on('click', () => {
     // random number generator
     const random = Math.floor(Math.random() * 3) + 1;
     $dice.attr('src', `./img/dice-${random}.png`);
     $dice.show();
+    // check if random = 1 else playerChange
     if (random !== 1) {
-      round += random;
-      $(`.player--${active}--roll`).text(round);
+      roundScore += random;
+      $(`.player--${active}--roll`).text(roundScore);
     } else {
       playerChange();
     }
   });
 
-
-  // hold button click event
+  // hold button 
   $hold.on('click', () => {
-    
+    points[active] += roundScore;
+    console.log(points[active]);
+    $(`.${active}--points`).text(points[active]);
+
+    if (points[active] >= 21) {
+      alert('winner');
+    } else {
+      playerChange();
+    }
   });
 });
